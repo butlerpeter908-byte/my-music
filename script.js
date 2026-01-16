646da74e892223ccb5decf50542e2a0456074add
Butler peter <butlerpeter908@gmail.com>
2026-01-16 11:02:34 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const API_KEY = 'AIzaSyBkricU1Xd041GGKd5BUXEXxfYU6fUzVzY'; 
 let player;
 
-// API and Player initialization (Purana code)
+// YouTube API load karna
 var tag = document.createElement('script');
 tag.src = "https://www.youtube.com/iframe_api";
 var firstScriptTag = document.getElementsByTagName('script')[0];
@@ -10,65 +10,54 @@ firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 function onYouTubeIframeAPIReady() {
     player = new YT.Player('youtube-player', {
         height: '0', width: '0',
-        events: { 'onStateChange': (e) => { if(e.data === 0) playBtn.innerText = 'PLAY'; } }
+        events: { 
+            'onStateChange': (e) => { 
+                if(e.data === 1) document.getElementById('play-btn').innerHTML = '<i class="fa-solid fa-pause"></i>';
+                else document.getElementById('play-btn').innerHTML = '<i class="fa-solid fa-play"></i>';
+            } 
+        }
     });
 }
 
 const searchBtn = document.getElementById('search-btn');
 const resultsList = document.getElementById('results-list');
-const disk = document.getElementById('disk');
 const playBtn = document.getElementById('play-btn');
 
 searchBtn.addEventListener('click', async () => {
     const query = document.getElementById('search-input').value;
-    if(!query) return alert("Gaane ka naam likho!");
+    if(!query) return;
 
-    // MaxResults ko 5 kar diya hai taaki list dikhe
-    const url = `https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=5&q=${encodeURIComponent(query)}&type=video&key=${API_KEY}`;
+    const url = `https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=10&q=${encodeURIComponent(query)}&type=video&key=${API_KEY}`;
     
     try {
         const res = await fetch(url);
         const data = await res.json();
-        
-        resultsList.innerHTML = ""; // Purani list saaf karein
+        resultsList.innerHTML = ""; 
 
-        if(data.items && data.items.length > 0) {
+        if(data.items) {
             data.items.forEach(item => {
-                const videoId = item.id.videoId;
-                const title = item.snippet.title;
-                const thumb = item.snippet.thumbnails.default.url;
-
-                // Har gaane ke liye ek row banana
                 const div = document.createElement('div');
                 div.classList.add('result-item');
                 div.innerHTML = `
-                    <img src="${thumb}">
+                    <img src="${item.snippet.thumbnails.default.url}">
                     <div class="result-info">
-                        <h4>${title.substring(0, 30)}...</h4>
+                        <h4>${item.snippet.title}</h4>
+                        <p>${item.snippet.channelTitle}</p>
                     </div>
                 `;
-                
-                // Click karne par gaana bajega
                 div.onclick = () => {
-                    document.getElementById('title').innerText = title;
-                    disk.style.backgroundImage = `url('${item.snippet.thumbnails.high.url}')`;
-                    player.loadVideoById(videoId);
-                    playBtn.innerText = 'PAUSE';
-                    resultsList.innerHTML = ""; // Play hone ke baad list chhupa dein
+                    player.loadVideoById(item.id.videoId);
+                    document.getElementById('title').innerText = item.snippet.title;
+                    document.getElementById('artist').innerText = item.snippet.channelTitle;
+                    document.getElementById('disk').style.backgroundImage = `url('${item.snippet.thumbnails.high.url}')`;
                 };
-                
                 resultsList.appendChild(div);
             });
-        } else {
-            alert("Gaana nahi mila!");
         }
-    } catch (err) {
-        alert("Error!");
-    }
+    } catch (err) { alert("Quota Over!"); }
 });
 
-// Play/Pause button logic wahi rahega...
 playBtn.addEventListener('click', () => {
-    if (player.getPlayerState() === 1) { player.pauseVideo(); playBtn.innerText = 'PLAY'; }
-    else { player.playVideo(); playBtn.innerText = 'PAUSE'; }
+    if (player.getPlayerState() === 1) player.pauseVideo();
+    else player.playVideo();
 });
